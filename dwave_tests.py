92017e3fc6a200a9350e0706d033c6f104cb4ad0
Ernesto Acosta <eacostam@gmail.com>
2023-09-13 11:40:25 +0200

    update

--- dwave_tests.py
+++ dwave_tests.py
@@ -11,7 +11,7 @@ Created on Mon Sep 11 18:19:34 2023
 from dwave.cloud import Client
 
 # Replace 'YOUR_API_TOKEN' with your D-Wave API token
-client = Client.from_config(token='DEV-9497f5036e9b82253b880fd0562ae8386487f9cd')
+client = Client.from_config(token='xxx')
 solver = client.get_solver()
 
 qubo = {(0, 0): -1, (1, 1): -1, (0, 1): 2}
